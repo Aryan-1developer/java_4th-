c42ff55e238061a4f301d00ef83fedd4fb32f029
Aryan Singh <aryan82004@gmail.com>
2026-05-10 15:53:03 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Enter the 1st  number");
+        int a = sc.nextInt();
+        
+        System.out.println("Enter the 2nd  number");
+        int b = sc.nextInt();
+        
+        System.out.println("Enter the 3rd  number");
+        int c = sc.nextInt();
+        
+        int biggest;
+        if(a>b && a>c){
+            biggest = a;
+        }
+        else if(b>a && b>c){
+            biggest = b;
+        }
+        else{
+            biggest = c;
+        }
+        System.out.println("biggest number :"+ biggest);
+        
+    }
+}
